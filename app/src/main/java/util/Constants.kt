07ea3217535faa1ea82c63e7afacec9b3500be65
daianac92 <dai.carlantini@gmail.com>
2022-05-20 15:54:57 -0300

    Add api model and implementation of Retrofit

---
+++ app/src/main/java/util/Constants.kt
@@ -0,0 +1,8 @@
+package util
+
+class Constants {
+    companion object {
+        const val API_KEY = "f1be062f944d4a818f7926eed7baef2c"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}

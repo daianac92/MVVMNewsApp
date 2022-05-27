a493cd5cbaadbafd19da8a17f4d670fe53145c59
daianac92 <dai.carlantini@gmail.com>
2022-05-27 10:58:48 -0300

    Add searching news implementation

--- app/src/main/java/util/Constants.kt
+++ app/src/main/java/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "f1be062f944d4a818f7926eed7baef2c"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }

6b53bf5a11119ffb34ce1a1edfd4f8a228258250
daianac92 <dai.carlantini@gmail.com>
2022-05-27 10:12:22 -0300

    Add request response impl

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -1,8 +1,37 @@
 package com.androiddevs.mvvmnewsapp.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import models.NewsResponse
 import repository.NewsRepository
+import retrofit2.Response
+import util.Resource
 
 class NewsViewModel(
     val newsRepository: NewsRepository
-) : ViewModel()
+) : ViewModel() {
+
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewsPage = 1
+
+    init {
+        getBreakingNews("ar")
+    }
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+}
--- app/src/main/java/repository/NewsRepository.kt
+++ app/src/main/java/repository/NewsRepository.kt
@@ -1,7 +1,11 @@
 package repository
 
+import api.RetrofitInstance
 import db.ArticleDataBase
 
 class NewsRepository(
     val db: ArticleDataBase
-)
+) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBrakingNews(countryCode, pageNumber)
+}
